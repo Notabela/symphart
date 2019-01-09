8bebe89de6e8d370e7b09394d4fcc3134b7dbd61
Notabela <daniel.obeng@macaulay.cuny.edu>
2019-01-09 16:45:25 -0500

    completed application

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -93,22 +93,19 @@
             $form = $this->createFormBuilder($article)
                 ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
                 ->add('body', TextareaType::class, array('required' => false, 'attr' => array('class' => 'form-control')))
-                ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+                ->add('save', SubmitType::class, array('label' => 'Update', 'attr' => array('class' => 'btn btn-primary mt-3')))
                 ->getForm();
 
             $form->handleRequest($request);
 
             if($form->isSubmitted() && $form->isValid()){
-                $article = $form->getData();
-
                 $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->persist($article);
                 $entityManager->flush();
 
                 return $this->redirectToRoute('article_list');
             }
 
-            return $this->render('articles/new.html.twig', array('form' => $form->createView()));
+            return $this->render('articles/edit.html.twig', array('form' => $form->createView()));
         }
 
 
